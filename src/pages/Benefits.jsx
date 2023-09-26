985cc3a1080e91a6ef99c5147321a52e934cf242
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-09-26 12:41:40 +0100

    completed benefits page

--- src/pages/Benefits.jsx
+++ src/pages/Benefits.jsx
@@ -1,36 +1,122 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import Topbar from '../components/Topbar'
 import { DataGrid } from '@mui/x-data-grid'
 import { Box, TextField } from '@mui/material'
+import { toast } from 'react-toastify'
+import { publicRequest } from '../functions/requestMethods'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 
 const Benefits = () => {
   // PAGE SIZE
   const [pageSize, setPageSize] = useState(100)
 
+  // TOAST ID
+  const toastId = useRef(null)
+
+  // API BENEFITS LIST
+  const [benefitsList, setBenefitsList] = useState([])
+
+  // API BENEFITS LIST
+  const [filteredBenefitsList, setfilteredBenefitsList] = useState([])
+
+  // PRODUCT ID
+  const userDetails = JSON.parse(sessionStorage.getItem('user'))
+  const productId = userDetails?.iD_Product
+  console.log(productId)
+
+  // FUNCTION TO GET BENEFITS
+  const getBenefits = async () => {
+    toastId.current = toast('Please wait...', {
+      autoClose: false,
+      isLoading: true,
+    })
+
+    try {
+      await publicRequest
+        .get(`/Product/Benefit?ID_Product=${productId}`)
+        .then((res) => {
+          console.log(res?.data[0])
+          const values = res?.data[0]?.benefits?.split(',')
+
+          const benefitsArray = values.map((value) => ({
+            benefit: value.trim(),
+          }))
+
+          console.log(benefitsArray)
+
+          setBenefitsList(benefitsArray)
+          setfilteredBenefitsList(benefitsArray)
+          toast.update(toastId.current, {
+            render: 'Fetched user benefits sucessfully!',
+            type: 'success',
+            autoClose: 2000,
+            isLoading: false,
+          })
+        })
+    } catch (error) {
+      console.log(error)
+      toast.update(toastId.current, {
+        type: 'error',
+        autoClose: 3000,
+        isLoading: false,
+        render: `${
+          error?.response?.data?.title ||
+          error?.response?.data?.description ||
+          error?.message ||
+          'Something went wrong, please try again'
+        }`,
+      })
+    }
+  }
+  // END OF FUNCTION TO GET BENEFITS
+
+  // FUNCTION TO FILTER BENEFITS
+  const handleFilterBenefits = (e) => {
+    const filteredBenefits = benefitsList?.filter((benefit) =>
+      benefit?.benefit
+        ?.toLowerCase()
+        ?.includes(e.target.value?.toLowerCase()?.trim())
+    )
+
+    console.log(filteredBenefits)
+
+    setfilteredBenefitsList(filteredBenefits)
+  }
+  // END OF FUNCTION TO FILTER BENEFITS
+
   //   COLUMN DATA
   const columns = [
     {
-      field: 'name',
-      headerName: 'Name',
-      width: 600,
-      headerClassName: 'super-app-theme--header',
-    },
-    {
-      field: 'address',
-      headerName: 'Address',
-      width: 500,
-      editable: false,
-      headerClassName: 'super-app-theme--header',
-    },
-
-    {
-      field: 'state',
-      headerName: 'State',
-      width: 150,
-      headerClassName: 'super-app-theme--header',
+      field: 'benefit',
+      headerName: 'Benefit Name ',
+      width: '1000',
+      // headerClassName: 'super-app-theme--header',
     },
   ]
+  // const columns = [
+  //   {
+  //     field: 'name',
+  //     headerName: 'Name',
+  //     width: 600,
+  //     headerClassName: 'super-app-theme--header',
+  //   },
+  //   {
+  //     field: 'address',
+  //     headerName: 'Address',
+  //     width: 500,
+  //     editable: false,
+  //     headerClassName: 'super-app-theme--header',
+  //   },
+
+  //   {
+  //     field: 'state',
+  //     headerName: 'State',
+  //     width: 150,
+  //     headerClassName: 'super-app-theme--header',
+  //   },
+  // ]
 
   const tableData = [
     { name: 'a HOSPITAL', address: 'Lagos', state: 'Lagos' },
@@ -63,8 +149,14 @@ const Benefits = () => {
     { name: 'a HOSPITAL', address: 'Lagos', state: 'Lagos' },
   ]
 
+  useEffect(() => {
+    getBenefits()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <div className='w-full h-screen flex '>
+      <ToastContainer />
       <Sidebar />
       <div className='flex-1 h-[100%] bg-slate-100 overflow-y-auto'>
         <Topbar title={'My Benefits'} />
@@ -76,6 +168,7 @@ const Benefits = () => {
               type='search'
               className='my-4'
               size='small'
+              onChange={handleFilterBenefits}
             />
           </div>
           <div className=' h-[400px] w-[90%]  max-sm:m-3 bg-white max-sm:w-[95%]'>
@@ -83,6 +176,7 @@ const Benefits = () => {
               sx={{
                 width: '100%',
                 height: '100%',
+
                 '& .super-app-theme--header': {
                   backgroundColor: 'rgba(29, 55, 136, 0.2)',
                 },
@@ -92,7 +186,7 @@ const Benefits = () => {
               }}
             >
               <DataGrid
-                rows={tableData}
+                rows={filteredBenefitsList}
                 columns={columns}
                 pageSize={pageSize}
                 onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
